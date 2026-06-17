73be863d6b77b407a0de114f5e08f1f6caee13dd
Ibrahim Al-Alali <iboalali@gmail.com>
2026-06-17 15:28:11 +0200

    Release v1.2.0: move layers (drag + numeric position)

--- model.js
+++ model.js
@@ -6,7 +6,7 @@
 // the flat derived model that all three renderers consume.
 
 export const SCHEMA_VERSION = 1;
-export const APP_VERSION = '1.1.0';
+export const APP_VERSION = '1.2.0';
 export const FORMAT_ID = 'icon-emboss';
 
 let idCounter = 0;
