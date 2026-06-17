cfd5805098777f2f8e22adf36ab4b16b287906c3
Ibrahim Al-Alali <iboalali@gmail.com>
2026-06-17 20:15:36 +0200

    Release v1.4.0: scale, group-scale, flip, telemetry, privacy link, import/id fixes

--- model.js
+++ model.js
@@ -6,7 +6,7 @@
 // the flat derived model that all three renderers consume.
 
 export const SCHEMA_VERSION = 1;
-export const APP_VERSION = '1.3.0';
+export const APP_VERSION = '1.4.0';
 export const FORMAT_ID = 'icon-emboss';
 
 let idCounter = 0;
