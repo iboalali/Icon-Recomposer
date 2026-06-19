d2f183302f6ffcafb0042c6bbe6d9d84c113694a
Ibrahim Al-Alali <iboalali@gmail.com>
2026-06-20 01:08:32 +0200

    Release v1.5.2

--- model.js
+++ model.js
@@ -6,7 +6,7 @@
 // the flat derived model that all three renderers consume.
 
 export const SCHEMA_VERSION = 2;
-export const APP_VERSION = '1.5.1';
+export const APP_VERSION = '1.5.2';
 export const FORMAT_ID = 'icon-emboss';
 
 let idCounter = 0;
